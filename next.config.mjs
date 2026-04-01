e4b0553b6335928d3ea5b49d1302d6a51b15156a
ewelinagebka1-spec <ewelina.gebka.1@gmail.com>
2026-04-01 09:58:52 +0200

    ignore TS and eslint errors during build

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
-  images: {
-    unoptimized: true,
-  },
-  basePath: "",
-  assetPrefix: "",
-  reactStrictMode: true,
-  poweredByHeader: false,
-  compress: true,
+    output: "export",
+    images: {
+          unoptimized: true,
+    },
+    typescript: {
+          ignoreBuildErrors: true,
+    },
+    eslint: {
+          ignoreDuringBuilds: true,
+    },
+    basePath: "",
+    assetPrefix: "",
+    reactStrictMode: true,
+    poweredByHeader: false,
+    compress: true,
 };
 
 export default nextConfig;
